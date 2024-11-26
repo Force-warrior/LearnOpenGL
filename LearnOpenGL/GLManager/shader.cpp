7221f9782e51090433d7d5ce49b87526ff0ae1aa
zgm <zgm@meitu.com>
2024-11-26 20:56:00 +0800

    [feature]添加错误检测

--- LearnOpenGL/GLManager/shader.cpp
+++ LearnOpenGL/GLManager/shader.cpp
@@ -8,6 +8,7 @@
 #include "shader.hpp"
 #include <fstream>
 #include <sstream>
+#include <iostream>
 
 Shader::Shader(const char* vertexPath, const char* fragmentPath)
 {
@@ -39,37 +40,59 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
         "#version 330 core\n"
         "layout (location=0) in vec3 aPos;\n"
         "layout (location=1) in vec3 aColor;\n"
-        "uniform float time;\n"
         "out vec3 color;\n"
         "void main()\n"
         "{\n"
-        "   float value = (sin(time)) / 5;\n"
         "   gl_Position = vec4(aPos, 1.0f);\n"
-        "   color = vec3(value) + aColor;\n"
+        "   color = aColor;\n"
         "}\0";
     
     const char * fragmentShaderSource =
         "#version 330 core\n"
         "out vec4 fragColor;\n"
         "in vec3 color;\n"
+        "uniform float time;\n"
         "void main()\n"
         "{\n"
-        "   fragColor = vec4(color, 1.0f);\n"
+        "   float intensity = (sin(time) + 1.0) / 2.0;\n"
+        "   fragColor = vec4(vec3(intensity) + color, 1.0f);\n"
         "}\0";
     
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
     
+    GLint success;
+    char info[1024] = {};
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 1024, NULL, info);
+        std::cout<<"Shader Compile Failed, info: "<<info<<std::endl;
+    }
+    
     
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 1024, NULL, info);
+        std::cout<<"Shader Compile Failed, info: "<<info<<std::endl;
+    }
     
     program_ = glCreateProgram();
     glAttachShader(program_, vertexShader);
     glAttachShader(program_, fragmentShader);
     glLinkProgram(program_);
+    
+    glGetProgramiv(program_, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program_, 1024, NULL, info);
+        std::cout<<"Shader Linked Failed, info: "<< info <<std::endl;
+    }
     
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
